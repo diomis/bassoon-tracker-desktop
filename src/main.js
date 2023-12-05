027a10d2061813bedf3b13070ff4db4238354207
denomis <simone.diotti97@gmail.com>
2023-12-05 23:34:13 +0100

    added app icons

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 1000,
         height: 800,
         autoHideMenuBar: true,
+        icon: process.platform !== 'darwin' ? 'src/icons/icon.ico' : 'src/icons/icon.icns'
     })
 
     win.loadFile('index.html')
